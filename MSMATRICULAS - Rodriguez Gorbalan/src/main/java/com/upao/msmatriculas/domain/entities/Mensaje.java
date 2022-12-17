ca3e45c189e9fd27f1d7c185a62815807f9a24f3
SandraMayli <112739316+SandraMayli@users.noreply.github.com>
2022-12-16 22:10:03 -0500

    Add files via upload

---
+++ MSMATRICULAS - Rodriguez Gorbalan/src/main/java/com/upao/msmatriculas/domain/entities/Mensaje.java
@@ -0,0 +1,21 @@
+package com.upao.msmatriculas.domain.entities;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "mensajes")
+@Getter
+@Setter
+public class Mensaje {
+
+    @Id
+    private String _id;
+
+    private String remitente;
+    private String destinatario;
+    private String asunto;
+    private String fecha;
+    private String contenido;
+}
---
+++ MSMATRICULAS - Rodriguez Gorbalan/src/main/java/com/upao/msmatriculas/domain/services/impl/MensajeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.upao.msmatriculas.domain.services.impl;
+
+import com.upao.msmatriculas.domain.entities.Mensaje;
+import com.upao.msmatriculas.domain.services.MensajeService;
+import com.upao.msmatriculas.infraestructure.repositories.implnosql.MensajeNosqlRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MensajeServiceImpl implements MensajeService {
+
+    @Autowired
+    MensajeNosqlRepositoryImpl mensajeNosqlRepository;
+
+    @Override
+    public boolean Registrar(Mensaje mensaje) {
+        return mensajeNosqlRepository.Registrar(mensaje);
+    }
+
+    @Override
+    public List<Mensaje> ListarTodos() {
+        return mensajeNosqlRepository.ListarTodos();
+    }
+
+    @Override
+    public List<Mensaje> ListarxRemitente(String remitente) {
+        return mensajeNosqlRepository.ListarxRemitente(remitente);
+    }
+
+    @Override
+    public Boolean Eliminar(String id) {
+        return mensajeNosqlRepository.Eliminar(id);
+    }
+
+    @Override
+    public Boolean Modificar(Mensaje mensaje) {
+        return mensajeNosqlRepository.Modificar(mensaje);
+    }
+}
